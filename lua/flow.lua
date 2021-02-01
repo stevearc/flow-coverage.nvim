da436b26dfafc103f086a506880980f4d56cdd94
Steven Arcangeli <stevearc@stevearc.com>
2021-02-01 09:34:33 -0800

    Simplify code since LspDiagnosticsChanged was fixed

--- lua/flow.lua
+++ lua/flow.lua
@@ -12,36 +12,22 @@ M._convert_coverage_to_diagnostics = function(coverage)
   return diagnostics
 end
 
-vim.lsp.handlers['textDocument/typeCoverage'] = function(err, _method, params, client_id, bufnr)
+M._handle_coverage = function(err, _method, params, client_id, bufnr)
   if err ~= nil then
     error(err)
     return
   end
-  local mode = vim.api.nvim_get_mode()
-  if string.sub(mode.mode, 1, 1) == 'i' then return end
-
   diagnostics = M._convert_coverage_to_diagnostics(params)
-  vim.lsp.diagnostic.set_signs(diagnostics, bufnr, client_id, nil, nil)
+  vim.lsp.diagnostic.set_signs(diagnostics, bufnr, client_id, nil, {priority = 8})
   vim.lsp.diagnostic.set_underline(diagnostics, bufnr, client_id, nil, nil)
 
   vim.api.nvim_buf_set_var(bufnr, 'flow_coverage_percent', params.coveredPercent)
 end
 
 M.on_attach = function(client)
-  local orig_callback = vim.lsp.handlers['textDocument/publishDiagnostics']
-  local new_callback = function(a1, a2, params, client_id, bufnr, config)
-    orig_callback(a1, a2, params, client_id, bufnr, config)
-
-    local errors = vim.lsp.diagnostic.get_count(bufnr, "Error")
-    if errors > 0 then return end
-
-    if client_id == client.id then
-      M.check_coverage()
-    end
-  end
-  vim.lsp.handlers['textDocument/publishDiagnostics'] = new_callback
+  vim.lsp.handlers['textDocument/typeCoverage'] = M._handle_coverage
+  vim.cmd [[autocmd User LspDiagnosticsChanged lua require'flow'.check_coverage()]]
   M.check_coverage()
-  vim.cmd [[autocmd InsertLeave <buffer> lua require'flow'.check_coverage()]]
 end
 
 M.check_coverage = function(bufnr)
