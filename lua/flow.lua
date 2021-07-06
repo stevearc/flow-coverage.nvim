de91fb1c789804e11814ea91b85e0939a602a923
Steven Arcangeli <stevearc@stevearc.com>
2021-07-06 12:37:42 -0700

    Simpler and more robust integration with diagnostics API

--- lua/flow.lua
+++ lua/flow.lua
@@ -2,6 +2,7 @@ local DiagnosticSeverity = vim.lsp.protocol.DiagnosticSeverity
 
 local M = {}
 local MESSAGE = "Uncovered code"
+local COVERAGE_CLIENT_ID = 1004
 
 local function convert_coverage_to_diagnostics(coverage)
   local diagnostics = coverage.uncoveredRanges
@@ -12,48 +13,17 @@ local function convert_coverage_to_diagnostics(coverage)
   return diagnostics
 end
 
-local function hash_diagnostic(diagnostic)
-  return string.format(
-    "%s:%s-%s:%s",
-    diagnostic.range.start.line,
-    diagnostic.range.start.character,
-    diagnostic.range["end"].line,
-    diagnostic.range["end"].character
-  )
-end
-
-local adding_type_diagnostics = false
-vim.lsp.handlers["textDocument/typeCoverage"] = function(err, _, params, client_id, bufnr)
+vim.lsp.handlers["textDocument/typeCoverage"] = function(err, _, params, _, bufnr)
   if err ~= nil then
     error(err)
     return
   end
 
   local type_diagnostics = convert_coverage_to_diagnostics(params)
-  local diagnostics = vim.lsp.diagnostic.get(bufnr, client_id)
-  local seen = {}
-  for _, diag in ipairs(diagnostics) do
-    if diag.message == MESSAGE then
-      seen[hash_diagnostic(diag)] = true
-    end
-  end
-  for _, diag in ipairs(type_diagnostics) do
-    -- Avoid adding duplicate type coverage diagnostics
-    if not seen[hash_diagnostic(diag)] then
-      table.insert(diagnostics, diag)
-    end
-  end
-
-  adding_type_diagnostics = true
-  pcall(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    nil,
-    nil,
-    { uri = vim.uri_from_bufnr(bufnr), diagnostics = diagnostics },
-    client_id
-  )
-  adding_type_diagnostics = false
-
+  -- We're using a fake client ID here so the coverage diagnostics don't collide
+  -- with the normal diagnostics
+  vim.lsp.diagnostic.save(type_diagnostics, bufnr, COVERAGE_CLIENT_ID)
+  vim.lsp.diagnostic.display(type_diagnostics, bufnr, COVERAGE_CLIENT_ID)
   vim.api.nvim_buf_set_var(bufnr, "flow_coverage_percent", params.coveredPercent)
 end
 
@@ -68,9 +38,6 @@ M.on_attach = function(_)
 end
 
 M.check_coverage = function(bufnr)
-  if adding_type_diagnostics then
-    return
-  end
   local params = {
     textDocument = {
       uri = vim.uri_from_bufnr(bufnr),
